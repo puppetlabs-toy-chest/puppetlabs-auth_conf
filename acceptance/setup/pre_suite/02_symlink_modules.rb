c07bdf15562e0b04c62a64cd1806ede9c491f615
Lindsey <lindsey@puppetlabs.com>
2012-11-13 16:21:44 -0800

    PE-308: be better about moving existing modules out of the way in test setup

--- acceptance/setup/pre_suite/02_symlink_modules.rb
+++ acceptance/setup/pre_suite/02_symlink_modules.rb
@@ -7,4 +7,9 @@ step "Linking auth_conf module in to modulepath"
 
 module_path = '/opt/puppet/share/puppet/modules'
 
-on master, "ln -f -s #{SourcePath}/auth_conf #{module_path}"
+# preserve the PE installed module (it'll get restored after the test run is complete)
+on master,
+   "test -d #{SourcePath}/auth_conf && mv #{module_path}/auth_conf #{module_path}/auth_conf.original",
+   {:acceptable_exit_codes => [0, 1]}
+
+on master, "ln -s #{SourcePath}/auth_conf #{module_path}"
